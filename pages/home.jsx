682b110a12c98b1d5b9978977c98c862f970b138
kyounga_laptop <kacomtr@gmail.com>
2024-10-17 18:47:55 +0900

    next.js 학습- 실습파일 업로드

---
+++ pages/home.jsx
@@ -0,0 +1,8 @@
+function Homepage() {
+  return (
+    <>
+      <div>hello Next.js</div>
+    </>
+  );
+}
+export default Homepage;
---
+++ pages/login.jsx
@@ -0,0 +1,9 @@
+function LoginPage() {
+  return (
+    <>
+      <div>로그인 페이지!</div>
+    </>
+  );
+}
+
+export default LoginPage;
